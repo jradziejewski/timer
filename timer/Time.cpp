ee3b93c4df617ad875e501b85fad057b8a03aa33
jradziejewski <261309@student.pwr.edu.pl>
2022-03-14 17:35:58 +0100

    build: add accessors

--- timer/Time.cpp
+++ timer/Time.cpp
@@ -1,1 +1,37 @@
 #include "Time.h"
+
+void Time::setSeconds(int number) {
+	if (number < 0 || number > 59) {
+		std::cout << "Bledny format. Sprobuj ponownie\n";
+		return;
+	}
+	else seconds = number;
+}
+
+void Time::setMinutes(int number) {
+	if (number < 0 || number > 59) {
+		std::cout << "Bledny format. Sproboj ponownie\n";
+		return;
+	}
+	else minutes = number;
+}
+
+void Time::setHours(int number) {
+	if (number < 0 || number > 23) {
+		std::cout << "Bledny format. Sproboj ponownie\n";
+		return;
+	}
+	else hours = number;
+}
+
+int Time::getSeconds() {
+	return seconds;
+}
+
+int Time::getMinutes() {
+	return minutes;
+}
+
+int Time::getHours() {
+	return hours;
+}
