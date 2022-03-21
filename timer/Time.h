6b86bb14a9ef27165e80236f5b0c4654c5779807
jradziejewski <261309@student.pwr.edu.pl>
2022-03-21 23:09:12 +0100

    Revert "feat: add + operator"
    This reverts commit 8819cb178faec85054a2c500632fd374dd5303e8.

--- timer/Time.h
+++ timer/Time.h
@@ -18,8 +18,5 @@ public:
 	//Methods
 	void printTime();
 	void refactorTime();
-
-	//Operator overloading
-	Time operator + (Time& _newTime);
 };
 
--- timer/main.cpp
+++ timer/main.cpp
@@ -3,10 +3,9 @@
 #include <math.h>
 
 int main() {
-	Time t1, t2, tsum;
-	t1.setSeconds(1240);
-	t2.setSeconds(6451);
-	tsum = t1 + t2;
-	tsum.printTime();
+	Time t1;
+	t1.setSeconds(654354300);
+	t1.refactorTime();
+	t1.printTime();
 	return 0;
 }
